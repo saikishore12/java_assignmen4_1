a6aac6fbbf8caf9095b057b8a8846988bfb97df9
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-24 21:31:38 +0530

    Assignment

---
+++ src/com/acadgild/Sample1.java
@@ -0,0 +1,9 @@
+package com.acadgild;
+
+public class Sample1 {
+
+protected static String  myName(){
+	//System.out.println("saikishore");
+	return "sai kishore";
+}
+}
---
+++ src/com/java/Sample2.java
@@ -0,0 +1,15 @@
+package com.java;
+import java.lang.reflect.Method;
+
+import com.acadgild.*;
+
+public class Sample2 extends Sample1{
+
+	public static void main(String[] args) {
+		System.out.println(myName());
+		
+	
+		
+	}
+
+}
